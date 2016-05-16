027dcdd954d59d11f7f39c2ea4f23cc511fd933a
oliverchen <oliverchen@CHENOLIVER.vxmt.local>
2016-05-16 16:45:20 +0800

    1ST not complete

---
+++ Mysample/src/com/example/mysample/Preview.java
@@ -0,0 +1,54 @@
+package com.example.mysample;
+
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+
+
+public class Preview extends ViewGroup implements SurfaceHolder.Callback {
+	SurfaceView mSurfaceView;
+	SurfaceHolder mHolder;
+	Camera mCamera = null;
+	
+	Preview(Context context){
+		super(context);
+		
+		mSurfaceView = new SurfaceView(context);
+		addView(mSurfaceView);
+		
+		mHolder = mSurfaceView.getHolder();
+		mHolder.addCallback(this);
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+public void setCamera(Camera camera){
+	if(mCamera == camera){return;}
+	
+	stopPreviewAndFreeCamera();
+	
+	mCamera = camera;
+	
+	if(mCamera != null){
+		List<Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+		mSupportedPreviewSizes = localSizes;
+		requestLayout();
+		try{
+			mCamera.setPreviewDisplay(mHolder);			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		mCamera.startPreview();
+	}
+}
+
+public void stopPreviewAndFreeCamera(){
+	
+}
+}
+
